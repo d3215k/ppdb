af885ad66c6536c965212d7e6683f87cde9521b9
Dede Iskandar <d3215k@gmail.com>
2024-03-16 15:22:24 +0700

    Update TesResource.php

--- app/Filament/Resources/TesResource.php
+++ app/Filament/Resources/TesResource.php
@@ -71,7 +71,7 @@ class TesResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                // Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
                 // Tables\Actions\BulkActionGroup::make([
